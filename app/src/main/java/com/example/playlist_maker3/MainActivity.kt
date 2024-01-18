fdf7556278f0c3c37b8f542d99d85ab0e00fed0f
DariaBandukova <dariabandukova@gmail.com>
2024-01-18 16:48:14 +0300

    Надеюсь в этот раз все получилось верно:(

--- app/src/main/java/com/example/playlist_maker3/MainActivity.kt
+++ app/src/main/java/com/example/playlist_maker3/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.playlist_maker3
-
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.google.android.material.button.MaterialButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button1 = findViewById<MaterialButton>(R.id.button1)
+        button1.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
+        }
+        val button2 = findViewById<MaterialButton>(R.id.button2)
+        button2.setOnClickListener {
+            val displayIntent2 = Intent(this, MediaActivity::class.java)
+            startActivity(displayIntent2)
+        }
+        val button3 = findViewById<MaterialButton>(R.id.button3)
+        button3.setOnClickListener {
+            val displayIntent3 = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent3)
+        }
     }
 }
---
+++ app/src/main/java/com/example/playlist_maker3/SettingsActivity.kt
@@ -0,0 +1,18 @@
+package com.example.playlist_maker3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.button.MaterialButton
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        val backButton = findViewById<MaterialButton>(R.id.backButton)
+        backButton.setOnClickListener {
+            val displayIntent4 = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent4)
+        }
+    }
+}
