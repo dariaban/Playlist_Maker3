bfc93d004b5449de62eb4d80d6d42e750a4c9cac
DariaBandukova <dariabandukova@gmail.com>
2024-02-20 00:39:45 +0300

    Привет!

--- app/src/main/java/com/example/playlist_maker3/SearchActivity.kt
+++ app/src/main/java/com/example/playlist_maker3/SearchActivity.kt
@@ -2,10 +2,64 @@ package com.example.playlist_maker3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import com.google.android.material.button.MaterialButton
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val backButton = findViewById<MaterialButton>(R.id.backButton)
+        backButton.setOnClickListener {
+            finish()
+        }
+        val inputEditText: EditText = findViewById(R.id.search_bar)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        fun clearButtonVisibility(s: CharSequence?): Int {
+            return if (s.isNullOrEmpty()) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
+        }
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+        inputEditText.setText(savedValue)
     }
+
+
+    private var savedValue: String = DEF_VALUE
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_VALUE, savedValue)
+    }
+
+    companion object {
+        const val SEARCH_VALUE = "SEARCH_VALUE"
+        const val DEF_VALUE = " "
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedValue = savedInstanceState.getString(SEARCH_VALUE, DEF_VALUE)
+
+    }
+
 }
--- app/src/main/java/com/example/playlist_maker3/SettingsActivity.kt
+++ app/src/main/java/com/example/playlist_maker3/SettingsActivity.kt
@@ -1,9 +1,15 @@
 package com.example.playlist_maker3
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.materialswitch.MaterialSwitch
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +19,37 @@ class SettingsActivity : AppCompatActivity() {
         backButton.setOnClickListener {
             finish()
         }
+        val shareButton = findViewById<MaterialButton>(R.id.share_button)
+        shareButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.setType("text/x-uri")
+            Intent.createChooser(shareIntent,null)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "@strings/android_url")
+            startActivity(shareIntent)
+        }
+        val supportButton = findViewById<MaterialButton>(R.id.button_support)
+        supportButton.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SEND)
+            supportIntent.setType("text/plain")
+            supportIntent.setPackage("com.google.android.gm")
+            supportIntent.putExtra(Intent.EXTRA_TEXT,"@strings/support_text")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL,"@strings/support_email")
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT,"@strings/support_subject")
+            startActivity(supportIntent)
+        }
+        val agreementButton = findViewById<MaterialButton>(R.id.button_agreement)
+        agreementButton.setOnClickListener {
+            val agreementIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.yandex.ru/legal/practicum_offer"))
+            startActivity(agreementIntent)
+        }
+        val switchTheme = findViewById<SwitchMaterial>(R.id.button_switch)
+        switchTheme.setOnCheckedChangeListener { _, b ->
+            if (b) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
     }
 }
