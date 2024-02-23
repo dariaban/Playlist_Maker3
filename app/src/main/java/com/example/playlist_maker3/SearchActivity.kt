20d669e93ea70413fceb25e854dfb87007d39108
DariaBandukova <dariabandukova@gmail.com>
2024-02-23 23:41:55 +0300

    Revert "Привет! Насчет кнопки очистить. В макете она вообще не видна, ориентировалась на размеры view. С onSaveIstanceState вроде разобралась."
    This reverts commit fa9e6bf511abf047034a89920386bcb87cea9172.

--- app/src/main/java/com/example/playlist_maker3/SearchActivity.kt
+++ app/src/main/java/com/example/playlist_maker3/SearchActivity.kt
@@ -1,64 +1,65 @@
 package com.example.playlist_maker3
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
-import androidx.core.view.isVisible
 import com.google.android.material.button.MaterialButton
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-        val inputEditText: EditText = findViewById(R.id.search_bar)
-        if (savedInstanceState != null) {
-            inputEditText.setText(savedInstanceState.getString("EDIT_TEXT_KEY"))
-        }
         val backButton = findViewById<MaterialButton>(R.id.backButton)
         backButton.setOnClickListener {
             finish()
         }
+        val inputEditText: EditText = findViewById(R.id.search_bar)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
-
+        fun clearButtonVisibility(s: CharSequence?): Int {
+            return if (s.isNullOrEmpty()) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
+        }
         clearButton.setOnClickListener {
             inputEditText.setText("")
-            val inputMethodManager =
-                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-            inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
         }
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                clearButton.isVisible = !s.isNullOrEmpty()
+                clearButton.visibility = clearButtonVisibility(s)
             }
 
             override fun afterTextChanged(s: Editable?) {
-
             }
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
-
+        inputEditText.setText(savedValue)
     }
 
+
+    private var savedValue: String = DEF_VALUE
     override fun onSaveInstanceState(outState: Bundle) {
-        val editText = findViewById<EditText>(R.id.search_bar)
-        outState.putString("EDIT_TEXT_KEY", editText.text.toString())
         super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_VALUE, savedValue)
+    }
+
+    companion object {
+        const val SEARCH_VALUE = "SEARCH_VALUE"
+        const val DEF_VALUE = " "
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        savedInstanceState.getString("EDIT_TEXT_KEY").toString()
+        savedValue = savedInstanceState.getString(SEARCH_VALUE, DEF_VALUE)
+
     }
+
 }
-
-
-
