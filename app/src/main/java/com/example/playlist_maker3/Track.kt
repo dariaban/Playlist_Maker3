a5af0e56c226eab86b093013aceb498e6edda336
DariaBandukova <dariabandukova@gmail.com>
2024-10-02 18:13:45 +0300

    Привет! Вроде, все работает

--- app/src/main/java/com/example/playlist_maker3/Track.kt
+++ app/src/main/java/com/example/playlist_maker3/Track.kt
@@ -6,6 +6,7 @@ data class Track(
     val trackId: Int,
     val artistName: String,
     val collectionName: String,
+    val previewUrl: String,
     val artworkUrl100: String,
     val trackName: String,
     val releaseDate: String,
